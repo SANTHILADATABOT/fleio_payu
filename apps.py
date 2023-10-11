d9cf9a4719a3c3e4111daa4e484cb3948b4506a2
Santhila <36661278+SANTHILADATABOT@users.noreply.github.com>
2023-10-11 09:16:48 +0530

    Update apps.py
    Modified the apps.py file by GIRISH dated 11-10-2023 09:15

--- apps.py
+++ apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 
 class PayUConfig(AppConfig):
     name = "fleio.billing.gateways.payu"
-    verbose_name = _("PayU")
+    verbose_name = _("Razorpay")
     fleio_module_type = 'payment_gateway'
     module_settings = {'capabilities': {
         'can_process_payments': True,
